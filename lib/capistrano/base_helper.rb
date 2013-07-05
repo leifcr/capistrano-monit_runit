ec7bd278af379547ee0a171084d54ea6ea7efbd7
Leif Ringstad <leifcr@gmail.com>
2013-07-05 15:39:19 +0200

    move files to lib

---
+++ lib/capistrano/base_helper.rb
@@ -0,0 +1,11 @@
+def try_require(library)
+  begin
+    require "#{library}"
+  rescue LoadError => e
+    puts "Capistrano / Base Helper: Cannot load library: #{library} Error: #{e}"
+  end
+end
+
+try_require 'capistrano/base_helper/base_helper'
+try_require 'capistrano/base_helper/runit_base'
+try_require 'capistrano/base_helper/monit_base'
