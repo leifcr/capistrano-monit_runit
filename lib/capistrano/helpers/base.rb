11a52430c043c2bfef51534eb6a0f3cd170cf775
Leif Ringstad <leifcr@gmail.com>
2015-03-12 13:24:11 +0100

    Fix module structure

--- lib/capistrano/helpers/base.rb
+++ lib/capistrano/helpers/base.rb
@@ -1,56 +1,58 @@
 module Capistrano
-  ##
-  # Helper functions for both runit and monit
-  module BaseHelper
-    def user_app_env_underscore
-      "#{fetch(:user)}_#{fetch(:application)}_#{environment}"
-    end
-
-    def user_app_env_underscore_short
-      "#{fetch(:user)[0...1]}_#{environment[0...1]}_#{fetch(:application)}"
-    end
-
-    def user_app_env_underscore_short_char_safe
-      user_app_env_underscore_short.gsub!('-', '_')
-    end
-
+  module Helpers
     ##
-    # Automatically sets the environment based on presence of
-    # :stage (multistage)
-    # :rails_env
-    # RAILS_ENV variable;
-    #
-    # Defaults to "production" if not found
-    #
-    def environment # rubocop:disable Metrics/MethodLength
-      if exists?(:rails_env)
-        fetch(:rails_env)
-      elsif exists?(:rack_env)
-        fetch(:rack_env)
-      elsif exists?(:stage)
-        fetch(:stage)
-      else
-        info '---------------------------------------------------------------'
-        info '- Stage, rack or rails environment isn\'t set in               -'
-        info '- :stage, :rails_env or :rack_env, defaulting to \'production\' -'
-        info '---------------------------------------------------------------'
-        'production'
+    # Helper functions for both runit and monit
+    module Base
+      def user_app_env_underscore
+        "#{fetch(:user)}_#{fetch(:application)}_#{environment}"
       end
-    end
 
-    def template_to_s(template_file)
-      fail "Cannot find templte #{template_file}" unless File.exist?(template_file)
-      ERB.new(File.read(config_file)).result(binding)
-    end
+      def user_app_env_underscore_short
+        "#{fetch(:user)[0...1]}_#{environment[0...1]}_#{fetch(:application)}"
+      end
 
-    ##
-    # Execute a rake taske using the proper env.
-    # run_rake db:migrate
-    #
-    def run_rake(task)
-      within(current_path) do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "#{task}"
+      def user_app_env_underscore_short_char_safe
+        user_app_env_underscore_short.gsub!('-', '_')
+      end
+
+      ##
+      # Automatically sets the environment based on presence of
+      # :stage (multistage)
+      # :rails_env
+      # RAILS_ENV variable;
+      #
+      # Defaults to "production" if not found
+      #
+      def environment # rubocop:disable Metrics/MethodLength
+        if exists?(:rails_env)
+          fetch(:rails_env)
+        elsif exists?(:rack_env)
+          fetch(:rack_env)
+        elsif exists?(:stage)
+          fetch(:stage)
+        else
+          info '---------------------------------------------------------------'
+          info '- Stage, rack or rails environment isn\'t set in               -'
+          info '- :stage, :rails_env or :rack_env, defaulting to \'production\' -'
+          info '---------------------------------------------------------------'
+          'production'
+        end
+      end
+
+      def template_to_s(template_file)
+        fail "Cannot find templte #{template_file}" unless File.exist?(template_file)
+        ERB.new(File.read(config_file)).result(binding)
+      end
+
+      ##
+      # Execute a rake taske using the proper env.
+      # run_rake db:migrate
+      #
+      def run_rake(task)
+        within(current_path) do
+          with rails_env: fetch(:rails_env) do
+            execute :rake, "#{task}"
+          end
         end
       end
     end
--- lib/capistrano/helpers/runit.rb
+++ lib/capistrano/helpers/runit.rb
@@ -1,89 +1,91 @@
 module Capistrano
-  module RunitHelper
-    # Any command sent to this function controls _all_ services related to the app
-    def runit_app_services_control(command)
-      return unless test("[ ! -h #{runit_etc_service_app_symlink_name} ]")
-      execute :sudo, :sv, "#{command} #{runit_etc_service_app_symlink_name}"
-    end
-
-    # Begin - single service control functions
-
-    # def start_service_once(service_name)
-    #   control_service(service_name, "once")
-    # end
-
-    # def start_service(service_name)
-    #   control_service(service_name, "start")
-    # end
-
-    # def stop_service(service_name)
-    #   control_service(service_name, "stop")
-    # end
-
-    # def restart_service(service_name)
-    #   control_service(service_name, "restart")
-    # end
-
-    def control_service(service_name, command, arguments, _ignore_error = false)
-      return unless test "[ ! -h #{runit_service_path(service_name)}/run ]"
-      execute :sv, "#{arguments} #{command} #{runit_service_path(service_name)}"
-    end
-
-    # Will not check if the service exists before trying to force it down
-    def force_control_service(service_name, command, arguments, _ignore_error = false)
-      execute :sv, "#{arguments} #{command} #{runit_service_path(service_name)}"
-    end
-
-    def disable_service(service_name)
-      force_control_service(service_name, 'force-stop', '', true) # force-stop the service before disabling it
-      within(runit_service_path(service_name)) do
-        execute :rm, '-f ./run' if test '[ ! -h ./run ]'
-        execute :rm, '-f ./finish' if test '[ ! -h ./finish ]'
-      end
-    end
-
-    def enable_service(service_name)
-      within(runit_service_path(service_name)) do
-        execute :ln, "-sf #{runit_service_run_config_file} ./run" if test '[ -h ./run ]'
-        execute :ln, "-sf #{runit_service_finish_config_file} ./finish" if test '[ -h ./finish ]'
-      end
-    end
-
-    def purge_service(service_name)
-      execute :rm, "-rf #{runit_service_path(service_name)}"
-    end
-
-    def runit_set_executable_files(service_name) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-      if test("[ -f '#{runit_service_run_config_file(service_name)}']")
-        execute :chmod, 'u+x runit_service_run_config_file(service_name)'
-        execute :chmod, 'g+x runit_service_run_config_file(service_name)'
-      end
-      if test("[ -f '#{runit_service_finish_config_file(service_name)}']")
-        execute :chmod, 'u+x runit_service_finish_config_file(service_name)'
-        execute :chmod, 'g+x runit_service_finish_config_file(service_name)'
+  module Helpers
+    module Runit
+      # Any command sent to this function controls _all_ services related to the app
+      def runit_app_services_control(command)
+        return unless test("[ ! -h #{runit_etc_service_app_symlink_name} ]")
+        execute :sudo, :sv, "#{command} #{runit_etc_service_app_symlink_name}"
       end
 
-      if test("[ -f '#{runit_service_log_run_file(service_name)}']")
-        execute :chmod, 'u+x runit_service_log_run_file(service_name)'
-        execute :chmod, 'g+x runit_service_log_run_file(service_name)'
+      # Begin - single service control functions
+
+      # def start_service_once(service_name)
+      #   control_service(service_name, "once")
+      # end
+
+      # def start_service(service_name)
+      #   control_service(service_name, "start")
+      # end
+
+      # def stop_service(service_name)
+      #   control_service(service_name, "stop")
+      # end
+
+      # def restart_service(service_name)
+      #   control_service(service_name, "restart")
+      # end
+
+      def control_service(service_name, command, arguments, _ignore_error = false)
+        return unless test "[ ! -h #{runit_service_path(service_name)}/run ]"
+        execute :sv, "#{arguments} #{command} #{runit_service_path(service_name)}"
       end
 
-      if test("[ -d '#{runit_service_control_path(service_name)}']") # rubocop:disable Style/GuardClause
-        execute :chmod, 'u+x -R runit_service_control_path(service_name)'
-        execute :chmod, 'g+x -R runit_service_control_path(service_name)'
+      # Will not check if the service exists before trying to force it down
+      def force_control_service(service_name, command, arguments, _ignore_error = false)
+        execute :sv, "#{arguments} #{command} #{runit_service_path(service_name)}"
       end
-    end
 
-    # End - single service control functions
+      def disable_service(service_name)
+        force_control_service(service_name, 'force-stop', '', true) # force-stop the service before disabling it
+        within(runit_service_path(service_name)) do
+          execute :rm, '-f ./run' if test '[ ! -h ./run ]'
+          execute :rm, '-f ./finish' if test '[ ! -h ./finish ]'
+        end
+      end
 
-    def runit_setup_log_folders_and_permissions(log_path, user = nil, group = nil)
-      user  = fetch(:user) if user.nil?
-      group = 'syslog' if group.nil?
-      # Have to sudo in case path is in /var/log
-      execute :sudo, :mkdir, "-p #{log_path}"
-      execute :sudo, :chown, "-R #{user}:#{group} #{log_path}"
-      execute :sudo, :chmod, "u+w #{log_path}"
-      execute :sudo, :chmod, "g+w #{log_path}"
+      def enable_service(service_name)
+        within(runit_service_path(service_name)) do
+          execute :ln, "-sf #{runit_service_run_config_file} ./run" if test '[ -h ./run ]'
+          execute :ln, "-sf #{runit_service_finish_config_file} ./finish" if test '[ -h ./finish ]'
+        end
+      end
+
+      def purge_service(service_name)
+        execute :rm, "-rf #{runit_service_path(service_name)}"
+      end
+
+      def runit_set_executable_files(service_name) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+        if test("[ -f '#{runit_service_run_config_file(service_name)}']")
+          execute :chmod, 'u+x runit_service_run_config_file(service_name)'
+          execute :chmod, 'g+x runit_service_run_config_file(service_name)'
+        end
+        if test("[ -f '#{runit_service_finish_config_file(service_name)}']")
+          execute :chmod, 'u+x runit_service_finish_config_file(service_name)'
+          execute :chmod, 'g+x runit_service_finish_config_file(service_name)'
+        end
+
+        if test("[ -f '#{runit_service_log_run_file(service_name)}']")
+          execute :chmod, 'u+x runit_service_log_run_file(service_name)'
+          execute :chmod, 'g+x runit_service_log_run_file(service_name)'
+        end
+
+        if test("[ -d '#{runit_service_control_path(service_name)}']") # rubocop:disable Style/GuardClause
+          execute :chmod, 'u+x -R runit_service_control_path(service_name)'
+          execute :chmod, 'g+x -R runit_service_control_path(service_name)'
+        end
+      end
+
+      # End - single service control functions
+
+      def runit_setup_log_folders_and_permissions(log_path, user = nil, group = nil)
+        user  = fetch(:user) if user.nil?
+        group = 'syslog' if group.nil?
+        # Have to sudo in case path is in /var/log
+        execute :sudo, :mkdir, "-p #{log_path}"
+        execute :sudo, :chown, "-R #{user}:#{group} #{log_path}"
+        execute :sudo, :chmod, "u+w #{log_path}"
+        execute :sudo, :chmod, "g+w #{log_path}"
+      end
     end
   end
 end
