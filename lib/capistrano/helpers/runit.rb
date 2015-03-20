cdeb411caac6fecfac0425b2b8453f4c99f7e07a
Leif Ringstad <leifcr@gmail.com>
2015-03-20 09:16:41 +0100

    Runit fixes

--- lib/capistrano/helpers/runit.rb
+++ lib/capistrano/helpers/runit.rb
@@ -3,7 +3,7 @@ module Capistrano
     module Runit
       # Any command sent to this function controls _all_ services related to the app
       def runit_app_services_control(command)
-        return unless test("[ ! -h #{runit_etc_service_app_symlink_name} ]")
+        return unless test("[ ! -h '#{runit_etc_service_app_symlink_name}' ]")
         execute :sudo, :sv, "#{command} #{runit_etc_service_app_symlink_name}"
       end
 
@@ -25,8 +25,8 @@ module Capistrano
       #   control_service(service_name, "restart")
       # end
 
-      def control_service(service_name, command, arguments, _ignore_error = false)
-        return unless test "[ ! -h #{runit_service_path(service_name)}/run ]"
+      def control_service(service_name, command, arguments = '', _ignore_error = false)
+        return unless test "[ -h '#{runit_service_path(service_name)}/run' ]"
         execute :sv, "#{arguments} #{command} #{runit_service_path(service_name)}"
       end
 
@@ -36,17 +36,20 @@ module Capistrano
       end
 
       def disable_service(service_name)
-        force_control_service(service_name, 'force-stop', '', true) # force-stop the service before disabling it
+        begin
+          force_control_service(service_name, 'force-stop', '', true) # force-stop the service before disabling it
+        rescue
+        end
         within(runit_service_path(service_name)) do
-          execute :rm, '-f ./run' if test '[ ! -h ./run ]'
-          execute :rm, '-f ./finish' if test '[ ! -h ./finish ]'
+          execute :rm, '-f ./run' if test "[ -h '#{runit_service_run_file(service_name)}' ]"
+          execute :rm, '-f ./finish' if test "[ -h '#{runit_service_finish_file(service_name)}' ]"
         end
       end
 
       def enable_service(service_name)
         within(runit_service_path(service_name)) do
-          execute :ln, "-sf #{runit_service_run_config_file} ./run" if test '[ -h ./run ]'
-          execute :ln, "-sf #{runit_service_finish_config_file} ./finish" if test '[ -h ./finish ]'
+          execute :ln, "-sf #{runit_service_run_config_file(service_name)} ./run" if test "[ ! -h '#{runit_service_run_file(service_name)}' ]"
+          execute :ln, "-sf #{runit_service_finish_config_file(service_name)} ./finish" if test "[ ! -h '#{runit_service_finish_file(service_name)}' ]"
         end
       end
 
@@ -55,18 +58,18 @@ module Capistrano
       end
 
       def runit_set_executable_files(service_name) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-        if test("[ -f '#{runit_service_run_config_file(service_name)}']")
+        if test("[ -f '#{runit_service_run_config_file(service_name)}' ]")
           execute :chmod, "775 #{runit_service_run_config_file(service_name)}"
         end
-        if test("[ -f '#{runit_service_finish_config_file(service_name)}']")
-          execute :chmod, "775 #{runit_service_finish_config_file(service_name)}"
+        if test("[ -f '#{runit_service_finish_config_file(service_name)}' ]")
+          execute :chmod, "775 #{runit_service_finish_config_file (service_name)}"
         end
 
-        if test("[ -f '#{runit_service_log_run_file(service_name)}']")
+        if test("[ -f '#{runit_service_log_run_file(service_name)}' ]")
           execute :chmod, "775 #{runit_service_log_run_file(service_name)}"
         end
 
-        if test("[ -d '#{runit_service_control_path(service_name)}']") # rubocop:disable Style/GuardClause
+        if test("[ -d '#{runit_service_control_path(service_name)}' ]") # rubocop:disable Style/GuardClause
           execute :chmod, "775 -R #{runit_service_control_path(service_name)}"
           # execute :chmod, 'u+x -R runit_service_control_path(service_name)'
           # execute :chmod, 'g+x -R runit_service_control_path(service_name)'
--- lib/capistrano/tasks/runit.rake
+++ lib/capistrano/tasks/runit.rake
@@ -71,16 +71,13 @@ namespace :runit do
       on roles(:app) do |host|
         # set :pw, ask("Sudo password", '')
         # execute :echo, "#{fetch(:pw)} | sudo -S ls /"
-        if test("[ ! -d '#{runit_user_base_path}' ]")
-          execute :sudo, :mkdir, "-p '#{runit_user_base_path}'"
-          execute :sudo, :chown, "#{fetch(:user)}:root '#{runit_user_base_path}'"
-          execute :sudo, :chmod, "6775 '#{runit_user_base_path}'"
-        end
-        if test("[ ! -d '#{runit_etc_service_path}' ]")
-          execute :sudo, :mkdir, "-p '#{runit_etc_service_path}'"
-          execute :sudo, :chown, "#{fetch(:user)}:root '#{runit_etc_service_path}'"
-          execute :sudo, :chmod, "6775 '#{runit_etc_service_path}'"
-        end
+        execute :sudo, :mkdir, "-p '#{runit_user_base_path}'" if test("[ ! -d '#{runit_user_base_path}' ]")
+        execute :sudo, :chown, "#{fetch(:user)}:root '#{runit_user_base_path}'"
+        execute :sudo, :chmod, "6775 '#{runit_user_base_path}'"
+
+        execute :sudo, :mkdir, "-p '#{runit_etc_service_path}'" if test("[ ! -d '#{runit_etc_service_path}' ]")
+        execute :sudo, :chown, "#{fetch(:user)}:root '#{runit_etc_service_path}'"
+        execute :sudo, :chmod, "6775 '#{runit_etc_service_path}'"
         within("#{runit_user_base_path}") do
           execute :mkdir, "-p #{app_env_folder}"
         end
@@ -113,6 +110,14 @@ namespace :runit do
         info "RUNIT: Created inital runit log services in #{runit_base_log_path} for #{fetch(:application)} on #{host}"
       end
     end
+    # '[INTERNAL] create /etc/sv folders and upload base templates needed'
+    task :runit_ensure_shared_sockets_and_pids_folders do
+      on roles(:app) do |host|
+        execute :mkdir, fetch(:pids_path) if test("[ ! -d #{fetch(:pids_path)} ]")
+        execute :mkdir, fetch(:sockets_path) if test("[ ! -d #{fetch(:sockets_path)} ]")
+      end
+    end
+
   end
 
   desc 'Disable runit services for application'
@@ -170,3 +175,4 @@ after 'deploy:updated',   'runit:enable'
 # after 'deploy:setup',     'runit:setup'
 after 'runit:setup',      'runit:setup:runit_create_app_services'
 after 'runit:setup:runit_create_app_services', 'runit:setup:runit_create_app_log_services'
+after 'runit:setup:runit_create_app_services', 'runit:setup:runit_ensure_shared_sockets_and_pids_folders'
